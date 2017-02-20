f140f12e07e280a0236e8f3c20f31bbde7856692
yangfei <yangfei86@vip.qq.com>
2017-02-20 19:33:49 +0800

    new redis method

---
+++ redis_number.go
@@ -0,0 +1,135 @@
+package redis_full
+
+import (
+	"time"
+)
+
+func (c RedisCache) INCR(key string, expires time.Duration) error {
+	conn := c.pool.Get()
+	defer conn.Close()
+
+	times := int(expires / time.Second)
+
+	if times > 0 {
+		_, err := conn.Do("INCR", key)
+		if err != nil {
+			return err
+		}
+		_, err = conn.Do("EXPIRE", key, times)
+		if err != nil {
+			return err
+		}
+	} else {
+		_, err := conn.Do("INCR", key)
+		if err != nil {
+			return err
+		}
+	}
+
+	return nil
+}
+
+func (c RedisCache) INCRBY(key string, value int, expires time.Duration) error {
+	conn := c.pool.Get()
+	defer conn.Close()
+
+	times := int(expires / time.Second)
+
+	if times > 0 {
+		_, err := conn.Do("INCRBY", key, value)
+		if err != nil {
+			return err
+		}
+		_, err = conn.Do("EXPIRE", key, times)
+		if err != nil {
+			return err
+		}
+	} else {
+		_, err := conn.Do("INCRBY", key, value)
+		if err != nil {
+			return err
+		}
+	}
+
+	return nil
+}
+
+func (c RedisCache) DECR(key string, expires time.Duration) error {
+	conn := c.pool.Get()
+	defer conn.Close()
+
+	times := int(expires / time.Second)
+
+	if times > 0 {
+		_, err := conn.Do("DECR", key)
+		if err != nil {
+			return err
+		}
+		_, err = conn.Do("EXPIRE", key, times)
+		if err != nil {
+			return err
+		}
+	} else {
+		_, err := conn.Do("DECR", key)
+		if err != nil {
+			return err
+		}
+	}
+
+	return nil
+}
+
+func (c RedisCache) DECRBY(key string, value int,expires time.Duration) error {
+	conn := c.pool.Get()
+	defer conn.Close()
+
+	times := int(expires / time.Second)
+
+	if times > 0 {
+		_, err := conn.Do("DECRBY", key, value)
+		if err != nil {
+			return err
+		}
+		_, err = conn.Do("EXPIRE", key, times)
+		if err != nil {
+			return err
+		}
+	} else {
+		_, err := conn.Do("DECRBY", key, value)
+		if err != nil {
+			return err
+		}
+	}
+
+	return nil
+}
+
+func (c RedisCache) INCRBYFLOAT(key string, value float64, expires time.Duration) error {
+	conn := c.pool.Get()
+	defer conn.Close()
+
+	times := int(expires / time.Second)
+
+	if times > 0 {
+		_, err := conn.Do("INCRBYFLOAT", key, value)
+		if err != nil {
+			return err
+		}
+		_, err = conn.Do("EXPIRE", key, times)
+		if err != nil {
+			return err
+		}
+	} else {
+		_, err := conn.Do("INCRBYFLOAT", key, value)
+		if err != nil {
+			return err
+		}
+	}
+
+	return nil
+}
+
+
+
+
+
