e418aade8d76d3845198511682d86f8b221f379e
yangfei <yangfei86@vip.qq.com>
2017-02-20 23:19:21 +0800

    new redis method

--- test_obj.go
+++ test_obj.go
@@ -10,15 +10,15 @@ type redisFactory func(*testing.T, time.Duration) RedisCache
 //TEST EXIST METHOD
 func CheckExists(t *testing.T, newredis redisFactory) {
 	redisDB := newredis(t, time.Hour)
-	if err := redisDB.Set("value", "1234", 2*time.Hour); err != nil {
-		t.Errorf("Error setting a value: %s", err)
+	if err := redisDB.Set("name", "tom", 2*time.Hour); err != nil {
+		t.Errorf("An unexpected error occurred: %s", err)
 	}
-	ok, err := redisDB.Exists("value")
+	ok, err := redisDB.Exists("name")
 	if err != nil {
-		t.Errorf("found an accident %v", err)
+		t.Errorf("An unexpected error occurred: %v", err)
 		return
 	}
 	if !ok {
-		t.Error("the key value is not exist!")
+		t.Error("The key is not exist!")
 	}
 }
